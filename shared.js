8756066a8acc0a363bd536bdb8d879faa5d6c368
Kevin <omallek@gmail.com>
2018-06-18 10:49:15 -0500

    adds js to create sidebar open and close functionality

--- shared.js
+++ shared.js
@@ -2,6 +2,9 @@ var backdrop = document.querySelector('.backdrop');
 var selectPlanButtons = document.querySelectorAll('.button');
 var modal = document.querySelector('.modal');
 var modalNoButton = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', function() {
@@ -11,7 +14,16 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
 }
 
 modalNoButton.addEventListener('click', closeModal);
-backdrop.addEventListener('click', closeModal);
+
+backdrop.addEventListener('click', function() {
+  mobileNav.style.display = 'none';
+  closeModal();
+});
+
+toggleButton.addEventListener('click', function() {
+  mobileNav.style.display = 'block';
+  backdrop.style.display = 'block';
+});
 
 function closeModal() {
   backdrop.style.display = 'none';
