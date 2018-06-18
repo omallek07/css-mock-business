40b8a67a8c05f3592c17a53356a3bcda0a701f2f
Kevin <omallek@gmail.com>
2018-06-18 09:50:24 -0500

    adds javascript file to handle button selection and modal display

---
+++ shared.js
@@ -0,0 +1,20 @@
+var backdrop = document.querySelector('.backdrop');
+var selectPlanButtons = document.querySelectorAll('.button');
+var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative');
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener('click', function() {
+    backdrop.style.display = 'block';
+    modal.style.display = 'block';
+  });
+}
+
+modalNoButton.addEventListener('click', closeModal);
+backdrop.addEventListener('click', closeModal);
+
+function closeModal() {
+  backdrop.style.display = 'none';
+  modal.style.display = 'none';
+}
+
